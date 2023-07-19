23b2d85f96aa097e86a29f4a713580db90d78348
cristianorregodev <cristian.duquew@gmail.com>
2023-07-19 12:23:02 -0500

    Hackwork (ABanner: QModifica texto de welcome

--- src/components/Home/Projects.jsx
+++ src/components/Home/Projects.jsx
@@ -3,6 +3,7 @@ import { getData } from '@/services/getData'
 import { SectionTitle } from '../SectionTitle'
 import { API_URL } from '@/config'
 import { ProjectCard } from '../ProjectCard'
+import Link from 'next/link'
 
 export const Projects = async () => {
     const { data } = await getData(`${API_URL}/projects`)
@@ -21,13 +22,12 @@ export const Projects = async () => {
                 ))}
             </div>
             <div className="flex justify-center w-full mt-8">
-                <a
+                <Link
                     className="flex justify-center items-center gap-2 text-dark-800 font-bold dark:text-dark-200 relative overflow-hidden z-10   before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-[2px] before:w-0 before:bg-dark-900 dark:before:bg-dark-50 before:rounded-lg   before:-z-10 before:transition-all before:duration-500 before:hover:w-full  transition-all duration-300  hover:text-dark-900  dark:hover:text-dark-50"
-                    href="https://docs.google.com/document/d/1qroZkOm9Bxvuf5LKPC12N-yTHao0oP0hR7HS_8qyZas/edit?usp=sharing"
-                    target="_blank"
+                    href="/projects"
                 >
                     Todos los proyectos <AiOutlineArrowRight />
-                </a>
+                </Link>
             </div>
         </section>
     )
