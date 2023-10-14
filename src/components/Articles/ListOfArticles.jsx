10d0b43e1c4a10d9b0a76af70b49a815878bf44f
cristianorregodev <cristian.duquew@gmail.com>
2023-10-14 14:19:34 -0500

    Fix (articles): Delete articles folders

--- src/components/Articles/ListOfArticles.jsx
+++
@@ -1,8 +0,0 @@
-import { getData } from '@/services/getData'
-import { API_URL } from '@/config'
-import { ArticleCard } from '../ArticleCard'
-
-export const ListOfArticles = async () => {
-    const { data } = await getData(`${API_URL}/posts`)
-    return data.map((article) => <ArticleCard key={article.id} article={article} />)
-}
