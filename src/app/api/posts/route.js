aa8a2cf71635c925fcb55b9bbd7e8e501e508d48
cristianorregodev <cristian.duquew@gmail.com>
2023-10-18 19:21:21 -0500

    Fix (api): Elimina carpeta API

--- src/app/api/posts/route.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-import { NextResponse } from 'next/server'
-export async function GET() {
-    console.log(process.cwd())
-    const post = fs.readFileSync(process.cwd() + `/src/posts/centrar-elementos-css.md`, 'utf-8')
-    return NextResponse.json({ post })
-}
