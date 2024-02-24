6e8e1296580c248ff7ac6a28e7dc1020f58b90fb
cristianorregodev <cristian.duquew@gmail.com>
2024-02-24 09:24:16 -0500

    Fix (border): Change border color opacity

--- src/components/Pill.jsx
+++ src/components/Pill.jsx
@@ -6,7 +6,7 @@ export const Pill = ({ children, url, externalUrl = false }) => {
         return (
             <Link
                 href={url}
-                className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100  dark:from-slate-800 dark:to-slate-950 hover:scale-105 transition-transform"
+                className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-dark-700 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100  dark:from-slate-800 dark:to-slate-950 hover:scale-105 transition-transform"
             >
                 {children}
             </Link>
@@ -15,7 +15,7 @@ export const Pill = ({ children, url, externalUrl = false }) => {
     return (
         <a
             href={url}
-            className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-white/10 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100  dark:from-slate-800 dark:to-slate-950 hover:scale-105 transition-transform"
+            className="rounded-full text-dark-800 dark:text-dark-200 border border-dark-300 dark:border-dark-700 flex justify-center items-center gap-x-2 py-1 px-2 md:py-2 text-xs md:text-base md:px-4 bg-gradient-to-r from-slate-50 to-slate-100  dark:from-slate-800 dark:to-slate-950 hover:scale-105 transition-transform"
             target="_blank"
             rel="noopener noreferrer"
         >
